f3dda3782463469b315b392e61f19455beca811c
sgholami <sgholami@uncc.edu>
2023-04-20 22:40:33 +0000

    update

--- Fina Project/Qlearning/main.py
+++ Fina Project/Qlearning/main.py
@@ -15,29 +15,30 @@ def plot_episode(plot, x_val, y_val, total_reward, plot_line, sub_plot_counter):
 
 if __name__ == "__main__":
     EPISODES = 10000
-
+    SAVE_FREQ = 1000
+    MIN_EPS = 0.1
     agent_plot = plt.figure()
     x_val, y_val = [], []
     sub_plot = agent_plot.add_subplot()
-    plt.xlabel("Episode #")
+    plt.xlabel("#Episode")
     plt.ylabel("Score")
-    plt.title("Scores vs Episode")
+    plt.title("Scores vs. Episode")
     plot_line, = sub_plot.plot(x_val, y_val)
     sub_plot.set_xlim([0, EPISODES])
-    sub_plot.set_ylim([-220, -80])
+    sub_plot.set_ylim([-300, 300])
 
-    env = gym.make("BipedalWalker-v3",  render_mode="human", autoreset=True, max_episode_steps=1000)
+    env = gym.make("BipedalWalker-v3", max_episode_steps=1000)
     env.reset()
 
-    total_exploring_episode = 400
+    total_exploring_episode = 1000
     # gamma is the discount factor
-    agent = Agent(discretize_levels=100, gamma=0.99, learning_rate=1e-3, total_exploring_episode=total_exploring_episode)
+    agent = Agent(discretize_levels=10, gamma=0.99, alpha=1e-3, total_exploring_episode=total_exploring_episode)
     for i in range(1, EPISODES + 1):
-        total_reward, failed, max_reached = agent.Q_learning(env, i)
+        total_reward, failed, max_reached, eps = agent.Q_learning(env, i, MIN_EPS)
         end_str = "Termination" if failed else "Truncation"
-        print(f"Episode {i} finished with '{end_str}', total reward: {total_reward}")
+        print(f"Episode {i} finished with '{end_str}', total reward: {total_reward}, epsilon: {eps}")
         plot_episode(agent_plot, x_val, y_val, total_reward, plot_line, i)
-        if i % total_exploring_episode == 0:
+        if i % SAVE_FREQ == 0:
             np.save(f'Q_{i}_episodes', np.array(dict(agent.Q)))
 
     print(f"All episodes finished!")
